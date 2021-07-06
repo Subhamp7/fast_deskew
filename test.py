9b027c9f494f442bc8169c3ed18c5a8b313afa41
Subham Prasad <subham306952@gmail.com>
2021-07-06 14:17:08 +0530

    Delete test.py

--- test.py
+++
@@ -1,7 +0,0 @@
-import cv2
-import fast_deskew
-INPUT_IMAGE_PATH = "input_image.png"
-img_input = cv2.imread(INPUT_IMAGE_PATH,cv2.IMREAD_GRAYSCALE)
-result_image, best_angle =fast_deskew(img_input)
-print('Best angle: {}'.format(best_angle))
-cv2.imwrite("result_image.png", result_image)
